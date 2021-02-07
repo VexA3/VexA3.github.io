ed96b74bf1b1b32027b0e34095cce0e1d94fe809
Billy <47019848+VexA3@users.noreply.github.com>
2021-02-07 07:22:21 -0600

    updated token

--- squad/Javascript/BM-Tac1.js
+++ squad/Javascript/BM-Tac1.js
@@ -6,7 +6,7 @@ window.onload = function () {
             type: 'GET',
             url: `https://api.battlemetrics.com/servers/3569099/relationships/sessions?at=2020-04-26T21:34:00Z&include=player`,
             headers: {
-                'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjI5ODdkMDEwZjZmNzg4NzAiLCJpYXQiOjE1ODc5MTI3MjMsIm5iZiI6MTU4NzkxMjcyMywiaXNzIjoiaHR0cHM6Ly93d3cuYmF0dGxlbWV0cmljcy5jb20iLCJzdWIiOiJ1cm46dXNlcjoxMjM3MDEifQ.EogtksOFfM-CAHQY8hXBi4lOJfz05AevjA3wizxtooM`
+                'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjA5NTgxYmE0MWFiODVkNjMiLCJpYXQiOjE2MTI3MDQwNTUsIm5iZiI6MTYxMjcwNDA1NSwiaXNzIjoiaHR0cHM6Ly93d3cuYmF0dGxlbWV0cmljcy5jb20iLCJzdWIiOiJ1cm46dXNlcjoxMjM3MDEifQ.KVxMN2q1fCHuNFboTI8pArn0TAldrxumCIAhhQSzBjs`
             },
             contentType: "application/json; charset-utf-8",
             dataType: "json",
